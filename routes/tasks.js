2a9fb4122bd0cd910aa798b273047f634932e759
Gabriel <gabriel.developer@outlook.com>
2020-04-19 01:07:31 -0300

    configurando metodo delete

--- routes/tasks.js
+++ routes/tasks.js
@@ -36,13 +36,20 @@ module.exports = app => {
                     }
                 })
                 .catch(error => {
-                    res.status(412).json({ msg: error.message })
-                })
+                    res.status(412).json({ msg: error.message });
+                });
         })
         .put((req, res) => {
-
+            Tasks.update(req.body, { where: req.params })
+                .then(result => res.sendStatus(204))
+                .catch(error => {
+                    res.status(412).json({ msg: error.message });
+                })
         })
         .delete((req, res) => {
-
+            Tasks.destroy({ where: req.params })
+                .then(result => res.sendStatus(204)).catch(error => {
+                    res.status(412).json({ msg: error.message });
+                })
         });
 };
