f6ccb22f26c2291b3a25e73891e985400e184fe9
Gabriel <gabriel.developer@outlook.com>
2020-04-19 01:02:24 -0300

    configurando metodo put

--- routes/tasks.js
+++ routes/tasks.js
@@ -8,16 +8,16 @@ module.exports = app => {
         })
         .get((req, res) => {
             Tasks.findAll({})
-            .then(result => res.json(result))
-            .catch(error => {
-                res.status(412).json({msg: error.message});
-            });
+                .then(result => res.json(result))
+                .catch(error => {
+                    res.status(412).json({ msg: error.message });
+                });
         })
         .post((req, res) => {
             Tasks.create(req.body)
                 .then(result => res.json(result))
                 .catch(error => {
-                    res.status(412).json({msg: error.message});
+                    res.status(412).json({ msg: error.message });
                 })
         })
 
@@ -27,7 +27,17 @@ module.exports = app => {
             next();
         })
         .get((req, res) => {
-            
+            Tasks.findOne({ where: req.params })
+                .then(result => {
+                    if (result) {
+                        res.json(result);
+                    } else {
+                        res.sedStatus(404);
+                    }
+                })
+                .catch(error => {
+                    res.status(412).json({ msg: error.message })
+                })
         })
         .put((req, res) => {
 
