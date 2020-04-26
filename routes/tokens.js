8d853bbf467bacd5dfa4e07dc6ecb9914acba32b
Gabriel <gabriel.developer@outlook.com>
2020-04-26 02:11:34 -0300

    trabalhando com autenticação de usuarios

--- routes/tokens.js
+++ routes/tokens.js
@@ -5,14 +5,14 @@ module.exports = app => {
     const Users = app.libs.config;
 
     app.post('/token', (req, res) => {
-        if(req.body.email && req.body.email.password){
+        if (req.body.email && req.body.email.password) {
             const email = req.body.email;
             const password = req.body.password;
 
-            Users.findOne({where: {email: email}})
+            Users.findOne({ where: { email: email } })
                 .then(user => {
-                    if (Users.isPaaword(user.password, password)){
-                        const payload = {id: user.id};
+                    if (Users.isPaaword(user.password, password)) {
+                        const payload = { id: user.id };
 
                         res.json({
                             token: jwt.encode(payload, cfg, jwtSecret)
@@ -20,7 +20,7 @@ module.exports = app => {
                     } else {
                         res.sendStatus(401);
                     }
-            }).catch(error => res.sendStatus(401));
+                }).catch(error => res.sendStatus(401));
         } else {
             res.sendSTatus(401);
         };
