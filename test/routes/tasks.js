e42814a1b6f56a209bef06d684d154823f6e47fe
Gabriel <gabriel.developer@outlook.com>
2020-05-01 18:07:03 -0300

    TDD apra testar endpoits

--- test/routes/tasks.js
+++ test/routes/tasks.js
@@ -1,7 +1,56 @@
-const jwt  = require('jwt-simple');
+const jwt = require('jwt-simple');
 describe('Routes: Tasks', () => {
     const Users = app.db.models.Users;
     const Tasks = app.db.models.Tasks;
 
     const jwtSecret = app.libs.config.jwtSecret;
-})
+    let token;
+    let fakeTask;
+    beforeEach(done => {
+
+    });
+
+    describe('GET /tasks', () => {
+        describe('status 200', () => {
+            it('returns a list of tasks', done => {
+
+            });
+        });
+    });
+
+    describe('POST /tasks/', () => {
+        describe('status 200', () => {
+            it('creates a new task', done => {
+
+            });
+        });
+    });
+    describe('GET /tasks/:id', () => {
+        describe('status 200', () => {
+            it('returns one task', done => {
+
+            });
+        });
+        describe('status 404', () => {
+            it('throws error when task not exist', done => {
+
+            });
+        });
+    });
+
+    describe('PUT /tasks/:id', () => {
+        describe('status 204', () => {
+            it('updates a task', done => {
+
+            });
+        });
+    });
+
+    describe('DELETE /tasks/:id', () => {
+        describe('status 204', () => {
+            it('removes a task', done => {
+
+            });
+        });
+    });
+});
